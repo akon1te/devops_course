9712ed9db6203b2a90d268ef66c39f017b8b4b04
Tim <miakov.timofey@huawei-partners.com>
2023-06-05 18:08:56 +0300

    update main for linter

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+"""Module providing functions for creating web page"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,4 +11,4 @@ def index():
     return render_template("cat.html", url='https://lookw.ru/8/828/1476173423-125.jpg')
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
