3d11268998b64e4dfad29429a15eb8eaa54d10b4
Tim <miakov.timofey@huawei-partners.com>
2023-06-05 17:28:03 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Render photo of cat
+    """
     return render_template("cat.html", url='https://lookw.ru/8/828/1476173423-125.jpg')
 
 if __name__ == '__main__':
